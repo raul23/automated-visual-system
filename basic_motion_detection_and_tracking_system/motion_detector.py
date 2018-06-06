c18c57c5e3dc14779627ad4410437d2da9f22cfa
Raul <rchfe23@gmail.com>
2018-06-06 10:08:45 -0400

    added some comments

--- basic_motion_detection_and_tracking_system/motion_detector.py
+++ basic_motion_detection_and_tracking_system/motion_detector.py
@@ -4,18 +4,20 @@ Reference: https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-t
 '''
 import argparse
 import datetime
-import imutils
+import imutils  # Set of convenience functions for image processing
 import time
 import cv2
 import ipdb
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
+# If --video switch not used, then webcam is used for motion detection
 ap.add_argument("-v", "--video", help="path to the video file")
+# --min-area: minimum size (pixels) for a region of an image to be
+# considered actual “motion”
 ap.add_argument("-a", "--min-area", type=int, default=100, help="minimum area size")
 args = vars(ap.parse_args())
 
-ipdb.set_trace()
 # if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
     camera = cv2.VideoCapture(0)
@@ -25,22 +27,26 @@ if args.get("video", None) is None:
 else:
     camera = cv2.VideoCapture(args["video"])
 
-# initialize the first frame in the video stream
+# initialize the first frame in the video file/webcam stream
+# NOTE: first frame can be used to model the background of the video stream
+# We `assume` that the first frame should not have motion, it should just
+# contain background
 firstFrame = None
 
 # loop over the frames of the video
 while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
+    # grab the current frame and initialize the occupied/unoccupied text
+    # grabbed (bool): indicates if `frame` was successfully read from the buffer
     (grabbed, frame) = camera.read()
-    text = "Unoccupied"
+    text = "Unoccupied"  # no activity in the room
 
     # if the frame could not be grabbed, then we have reached the end
     # of the video
     if not grabbed:
         break
 
-    # resize the frame, convert it to grayscale, and blur it
+    # Preprocessing: preprare current frame for motion analysis
+    # resize the frame to 500 pixels wide, convert it to grayscale, and blur it
     frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
@@ -50,13 +56,14 @@ while True:
         firstFrame = gray
         continue
 
-    # compute the absolute difference between the current frame and
-    # first frame
+    ### Start of motion detection and tracking ###
+
+    # compute the absolute difference between the current frame and first frame
     frameDelta = cv2.absdiff(firstFrame, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
+    # dilate the thresholded image to fill in holes, then find contours on
+    # thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
     (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                     cv2.CHAIN_APPROX_SIMPLE)
@@ -73,7 +80,7 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         text = "Occupied"
 
-    # draw the text and timestamp on the frame
+    # draw the text (top left) and timestamp (bottom left) on the frame
     cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
